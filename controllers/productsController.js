23f28eab486899c64949dd6cef364995e670487b
marcotristani <marco.tristani@hotmail.it>
2026-03-09 11:06:33 +0100

    creato controller  con index prodotti

---
+++ controllers/productsController.js
@@ -0,0 +1,9 @@
+//importo connsessione DB
+const connection = require("./../data/db");
+
+//definisco funzioni CRUD
+function index(req, res) {
+  res.send(<h1>sono il controller index di products</h1>);
+}
+
+module.exports = { index };
