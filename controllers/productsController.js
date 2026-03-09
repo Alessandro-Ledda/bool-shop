7ce6468a4f1b483e8da4f3bd15fe6318f80df9d8
marcotristani <marco.tristani@hotmail.it>
2026-03-09 11:21:37 +0100

    corretto errore

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ const connection = require("./../data/db");
 
 //definisco funzioni CRUD
 function index(req, res) {
-  res.send(<h1>sono il controller index di products</h1>);
+  res.send("<h1>sono il controller index di products</h1>");
 }
 
 module.exports = { index };
