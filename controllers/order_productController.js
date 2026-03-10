e8a0a34cb09663fd983c123c421397b28eb0d128
marcotristani <marco.tristani@hotmail.it>
2026-03-10 15:23:30 +0100

    add logica store, modify

--- controllers/order_productController.js
+++ controllers/order_productController.js
@@ -6,9 +6,10 @@ const connection = require("./../data/db");
 //INDEX per recuperare lista prodotti nell'ordine 1
 function index(req, res) {
   //definisco query sql
-  const sql = `SELECT *
-                 FROM order_product
-                 WHERE order_id = 1`;
+  const sql = `SELECT product_id,name,price,discount_percentage,unit_price,unit_quantity,image
+               FROM order_product
+               JOIN products ON order_product.product_id = products.id
+               WHERE order_id = 1`;
 
   //eseguo richiesta al DB
   connection.query(sql, (err, results) => {
@@ -18,33 +19,80 @@ function index(req, res) {
   });
 }
 
+//STORE per inserire un nuovo prodotto nell'ordine
+function store(req, res) {
+  //prendo i dati in ingresso
+  const { product_id, unit_quantity } = req.body;
+
+  //-----------------RECUPERO DA DB PREZZO E SCONTO DEL PRODOTTO------------------------
+  const sqlProduct = `SELECT price, discount_percentage
+                      FROM products
+                      WHERE id = ?`;
+
+  //eseguo query al DB
+  connection.query(sqlProduct, [product_id], (err, results) => {
+    //definisco product price come intero per utlizzarlo nelle operazioni di verifica
+    const product_price = parseInt(results[0].price);
+    const discount_percentage = parseInt(results[0].discount_percentage);
+
+    //definisco il prezzo finale per il singolo prodotto
+    let unit_price = product_price;
+
+    //se esiste il valore discount percentage, e quindi il prodotto è in promozione allora definisco il totate scontato
+    discount_percentage &&
+      (unit_price =
+        product_price - product_price * (discount_percentage / 100));
+
+    //definisco sql per andare a crearmi la nuova riga nell'ordine 1 con il nuovo prodotto
+    const sql = `INSERT INTO order_product (product_id, order_id, unit_quantity, unit_price)
+                VALUES (?, 1, ?, ?)`;
+
+    //eseguo richiesta al DB
+    connection.query(
+      sql,
+      [product_id, unit_quantity, unit_price],
+      (err, results) => {
+        if (err) return res.status(500).json({ error: "database not found" });
+
+        res.sendStatus(201);
+      },
+    );
+  });
+}
+
+//MODIFY per andare a modificare la quantita di un prodotto nell'ordine
+function modify(req, res) {
+  //recupero id del prodotto in cui andare a modificare la quantità nell'ordine
+  const product_id = parseInt(req.params.product_id);
+
+  //recupero quantità aggiornata da oggetto body
+  const { unit_quantity } = req.body;
+
+  //definisco sql per andare a crearmi la nuova riga nell'ordine 1 con il nuovo prodotto
+  const sql = `UPDATE order_product
+               SET unit_quantity = ?
+               WHERE product_id = ? AND order_id = 1`;
+
+  //eseguo richiesta al DB
+  connection.query(
+    sql,
+    [parseInt(unit_quantity), product_id],
+    (err, results) => {
+      if (err) return res.status(500).json({ error: "database not found" });
+
+      res.sendStatus(201);
+    },
+  );
+}
+
 // //DESTROY per eliminare uno o tutti i prodotti
-// function destroy(req, res) {
-//   //recupero id dell'elemento che voglio andare ad eliminare
-//   const id = parseInt(req.params);
-
-//   //definisco queryDelProduct_id se mi arriva il valore id del prodotto specifico da eliminare
-//   const queryDelProduct_id = id ? `AND product_id = ${id}` : "";
-
-//   //definisco queri sql
-//   const sql = `DELETE FROM order_product
-//                WHERE order_id = 1
-//                ${queryDelProduct_id}`;
-
-//   //eseguo richiesta al DB
-//   connection.query(sql, (err, results) => {
-//     if (err) return res.status(500).json({ error: "Database query failed" });
-
-//     res.sendStatus(204);
-//   });
-// }
-
 function destroy(req, res) {
   // recupero id dell'elemento da eliminare
-  const id = parseInt(req.params.id);
+  const product_id = parseInt(req.params.product_id);
 
   // definisco query aggiuntiva se arriva l'id
-  const queryDelProduct_id = id === 0 ? "" : `AND product_id = ${id}`;
+  // se viene passa 0 cone params allora elimino tutto altrimenti solo il prodotto con l'id passato
+  const queryDelProduct_id = product_id === 0 ? "" : `AND product_id = ?`;
 
   // query SQL
   const sql = `
@@ -54,7 +102,7 @@ function destroy(req, res) {
   `;
 
   // eseguo richiesta al DB
-  connection.query(sql, (err, results) => {
+  connection.query(sql, [product_id], (err, results) => {
     if (err) {
       return res.status(500).json({ error: "Database query failed" });
     }
@@ -63,4 +111,4 @@ function destroy(req, res) {
   });
 }
 
-module.exports = { index, destroy };
+module.exports = { index, destroy, store, modify };
--- routers/order_productRouter.js
+++ routers/order_productRouter.js
@@ -8,7 +8,19 @@ const order_productController = require("./../controllers/order_productControlle
 //rotta index
 order_productRouter.get("/order1", order_productController.index);
 
+//rotta store
+order_productRouter.post("/order1", order_productController.store);
+
+//rotta modify
+order_productRouter.patch(
+  "/order1/:product_id",
+  order_productController.modify,
+);
+
 //rotta destroy
-order_productRouter.delete("/order1/:id", order_productController.destroy);
+order_productRouter.delete(
+  "/order1/:product_id",
+  order_productController.destroy,
+);
 
 module.exports = order_productRouter;
