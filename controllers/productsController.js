f77f93a7b144d8f7d7b317e01278bf463f2f56de
marcotristani <marco.tristani@hotmail.it>
2026-03-11 11:05:02 +0100

    sistemati errori

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -8,9 +8,8 @@ const connection = require("./../data/db");
 function index(req, res) {
   //recuper valore chiave order per vedere ordinamento
   const order = req.query.order;
+  //recupero valore discount per selezionare solo i prodotti che hanno un discount
   const discount = req.query.discount;
-  //recuperiamo il valore per la chiave tag fornitaci nella query string normalizzata
-  const searched = req.query.searched.toLowerCase();
 
   //dichiaro variabile che andrà a costituirmi la query di ordinamento nella richiesta DB
   let orderBy = "RAND()";
@@ -26,6 +25,7 @@ function index(req, res) {
   //verifico se definita nell'endpoint la key discount per andare a recuperare dal DB solo prodotti in discount
   let ifDiscount = "";
   if (discount)
+    //se è uguale a true applico discount altrimenti qualsiasi altro vlore non lo accetto
     discount === "true" && (ifDiscount = "WHERE discount_percentage");
 
   //definisco query sql
@@ -38,14 +38,17 @@ function index(req, res) {
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: "database not found" });
 
+    //inizializzo array filtrato come tutto results
     let filteredSearched = results;
-    //se esiste un valore di query fornito allora applico il filtro
-    if (searched) {
-      //verifico se il tag arrivato è presente nell'array tags dell'oggetto posts e mi faccio tornare i post che soddisfano questa condizione
+    //se esiste un valore di query per searched allore eseguo logica filtraggio
+    if (req.query.searched) {
+      //recupero valore searched per filtrare i prodotti in base al nome e lo normalizzo
+      const searched = req.query.searched.toLowerCase();
+      //verifico se il searched arrivato è incluso nel nome del prodotto
       filteredSearched = results.filter((result) => {
         //normalizzo i valori da confrontare
-
         const normalName = result.name.toLowerCase();
+
         return normalName.includes(searched);
       });
     }
